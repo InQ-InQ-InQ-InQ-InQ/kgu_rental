6640dec4bb12c5eb05c1a657101780fbaad42c7d
yumyeonghan <mgtmh991013@gmail.com>
2022-03-25 21:36:18 +0900

    초기 세팅

---
+++ src/main/java/team3/kgurental/KguRentalApplication.java
@@ -0,0 +1,13 @@
+package team3.kgurental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KguRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KguRentalApplication.class, args);
+	}
+
+}
