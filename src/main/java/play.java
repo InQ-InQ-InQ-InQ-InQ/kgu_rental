abbc2c6d4319ad6a8e6efd6d9795a5076ff8592b
yumyeonghan <yumyeonghan@gmail.com>
2022-04-08 18:34:50 +0900

    초기 세팅

--- src/main/java/play.java
+++ src/main/java/play.java
@@ -2,6 +2,6 @@ public class play {
     public static void main(String[] args) {
         System.out.println("3팀");
         System.out.println("test2");
-        System.out.println("test3");
+        System.out.println("tes");
     }
 }
