87fc159cfb4711a5c6654eb7484149fc6ee04e09
Yujin Sung <rnlduadns98@naver.com>
2022-04-22 15:54:26 +0900

    Feature: item - user 간 Many to Many로 itemManager 관계 구성

--- src/main/java/team3/kgurental/model/entity/Item.java
+++ src/main/java/team3/kgurental/model/entity/Item.java
@@ -3,16 +3,26 @@ package team3.kgurental.model.entity;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter @Setter
 public class Item {
 
-    @Id @GeneratedValue
-    private Long itemId;
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="item_id")
+    private Long Id;
     private String itemName;
     private Long itemCount;
+    private Long term;
+
+    @ManyToMany
+    @JoinTable(name = "item_user",
+    joinColumns = @JoinColumn(name="item_id"),
+    inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<Item> item = new ArrayList<>();
+
 }
