94c8809d0c9c40151684c432d968b9708713b17f
HyeonJeong <58041212+HyeonJeong519@users.noreply.github.com>
2022-05-02 22:57:52 +0900

    [feat/item] 아이템 개수 수정 코드 추가

--- src/main/java/team3/kgurental/repository/ItemRepository.java
+++ src/main/java/team3/kgurental/repository/ItemRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import team3.kgurental.model.entity.Item;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
+    Item findItemById(Long id);
 }
